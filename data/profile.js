21d39a2f1e3e250f654f2e3914800ff52aea5985
Moses Amartey <yormengh@gmail.com>
2024-11-03 16:12:18 +0400

    resume app deployment

---
+++ data/profile.js
@@ -0,0 +1,142 @@
+const profileData = {
+  title: "Resume",
+  name: "Moses Amartey",
+  sub_title: "System Administrator and DevOps Engineer",
+  logoURL: "assets/images/dp.jpg",
+  about: {
+    intro: `DevOps Engineer with a focus on applying DevSecOps engineering principles to ensure secure software integration and delivery. Skilled in building and managing CI/CD pipelines, automating cloud infrastructure with IaC tools, and implementing robust monitoring and security practices. Committed to optimizing performance, leading incident management, and fostering a culture of collaboration and continuous improvement. Strong technical skills allow for enhanced efficiency, reliability, and high availability across software development platforms.`,
+    contact: {
+      email: "yormengh@gmail.com",
+      phone: "+233555910005",
+      address: "Tema, Accra - Ghana",
+    },
+  },
+  links: [
+    {
+      title: "LinkedIn",
+      src: "https://www.linkedin.com/in/moses-amartey",
+      iconClass: "fa-brands fa-linkedin-in",
+    },
+    {
+      title: "Github",
+      src: "https://github.com/yormengh",
+      iconClass: "fa-brands fa-github",
+    },
+  ],
+  skills: [
+    {
+      title: "Operating System",
+      value: "Linux (Ubuntu, RedHat)",
+    },
+    {
+      title: "Languages & Scripting",
+      value: "Bash, Python",
+    },
+    {
+      title: "Version Control",
+      value: "Git",
+    },
+    {
+      title: "CI/CD",
+      value: "Jenkins, GitHub Actions, ArgoCD, GitOps",
+    },
+    {
+      title: "Container & Orchestration",
+      value: "Docker, Kubernetes, Helm",
+    },
+    {
+      title: "Monitoring & Logging",
+      value: "Grafana, Prometheus, Loki",
+    },
+    {
+      title: "Infrastructure as Code (IaC)",
+      value: "Terraform, Pulumi",
+    },
+    {
+      title: "DevSecOps",
+      value: "Security integration in CI/CD, Incident management",
+    },
+  ],
+  experiences: [
+    {
+      organization: "JOMACS IT SOLUTION INC.",
+      title: "DevOps Engineer",
+      date: "February 2021 - Present",
+      details: [
+        `Designed and implemented containerization strategies using Docker and Kubernetes, resulting in a 20% improvement in resource utilization and management through enhanced collaboration and streamlined processes.`,
+        `Developed and maintained automated CI/CD pipelines that reduced deployment time by 50%, achieving a 99.9% deployment success rate across development, staging, and production environments.`,
+        `Spearheaded the transition to Ansible and Terraform for provisioning infrastructures, including EC2 instances, RDS databases, S3 buckets, and VPCs, resulting in a 33% reduction in manual configuration time and a 40% decrease in operational costs.`,
+        `Implemented GitHub Actions and Jenkins CI/CD pipelines with integrated security checks, as well as Prometheus and Grafana for real-time monitoring, achieving a 72-hour reduction in mean time to resolution for critical system incidents.`,
+      ],
+    },
+    {
+      organization: "PARBTECH SOLUTIONS",
+      title: "Linux System Engineer",
+      date: "January 2019 - January 2021",
+      details: [
+        `Streamlined deployment processes for software updates, resulting in a 21% reduction in downtime and a 30% increase in productivity.`,
+        `Enhanced system performance by optimizing Linux server configurations and implementing efficient monitoring tools.`,
+        `Collaborated with development teams to design and implement scalable infrastructure solutions that supported company growth and demand.`,
+        `Maintained high-availability environments by configuring load balancers, clustering solutions, and failover mechanisms, ensuring reliable and continuous service delivery.`,
+      ],
+    },
+  ],
+  projects: [
+    {
+      title: "DevOps Full-Stack Bank Application",
+      duration: "N/A",
+      desc: `Developed and deployed a full-stack bank application using CI/CD pipeline tools like Jenkins and GitHub Actions.`,
+    },
+    {
+      title: "Netflix Clone on AWS",
+      duration: "N/A",
+      desc: `Set up a DevSecOps environment with CI/CD, security integration, monitoring, and GitOps principles for a Netflix clone on AWS.`,
+    },
+    {
+      title: "Kubernetes Three-Tier Project",
+      duration: "N/A",
+      desc: `Built a three-tier architecture on AWS EKS using Terraform, ArgoCD, Helm, Prometheus, Grafana, and Jenkins.`,
+    },
+  ],
+  accomplishments: [
+    {
+      title: "Cloud Migration Expert",
+      desc: "Led the transition from on-premises setups to the cloud, achieving scalable, efficient operations.",
+    },
+    {
+      title: "CI/CD Pipeline Architect",
+      desc: "Designed scalable CI/CD pipelines, streamlining development and deployment processes.",
+    },
+    {
+      title: "Infrastructure Architect",
+      desc: "Built infrastructure solutions tailored to specific business needs.",
+    },
+    {
+      title: "Platform Transition Pro",
+      desc: "Led application migrations between platforms, optimizing performance and reliability.",
+    },
+    {
+      title: "Infrastructure as Code Advocate",
+      desc: "Skilled in codifying infrastructure using Terraform, simplifying deployment and management.",
+    },
+    {
+      title: "Security Champion",
+      desc: "Secured infrastructures and applications, delivering comprehensive security reporting to protect assets.",
+    },
+    {
+      title: "Cost Optimization Specialist",
+      desc: "Implemented strategies to reduce cloud costs, enhancing efficiency without sacrificing performance.",
+    },
+  ],
+  education: [
+    {
+      alma: "Evangelical Presbyterian University College",
+      duration: "N/A",
+      std: "Bachelor of Science: Business Administration",
+      score: "N/A",
+    },
+  ],
+  certifications: [],
+  events: [],
+};
+
